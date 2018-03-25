8dbefe51d3e89236ed185c6b6607694de928e69d
Cryptohelper1 <37479709+Cryptohelper1@users.noreply.github.com>
2018-03-25 13:34:15 -0400

    FIX PORTS

--- src/chainparamsbase.cpp
+++ src/chainparamsbase.cpp
@@ -22,7 +22,7 @@ public:
     CBaseMainParams()
     {
         networkID = CBaseChainParams::MAIN;
-        nRPCPort = 32458;
+        nRPCPort = 12458;
     }
 };
 static CBaseMainParams mainParams;
@@ -36,7 +36,7 @@ public:
     CBaseTestNetParams()
     {
         networkID = CBaseChainParams::TESTNET;
-        nRPCPort = 32459;
+        nRPCPort = 12459;
         strDataDir = "testnet4";
     }
 };
