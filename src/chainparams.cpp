620cdada30adcef4bb3d8a583824013975fcf921
altcuim <thien.atcvn@gmail.com>
2018-05-23 21:15:36 +0700

    modified:   contrib/seeds/nodes_main.txt 	modified:   contrib/seeds/nodes_test.txt 	modified:   src/chainparams.cpp 	modified:   src/chainparamsseeds.h

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -155,8 +155,8 @@ public:
 
 	if(!startNewChain){
 
-        assert(hashGenesisBlock == uint256("0x00000427198483f7776f4a6b1e4afaeaa9adeed6ceaf0d137747080f6665a442"));
-        assert(genesis.hashMerkleRoot == uint256("0x1fbd576f57ed6765eac3c705721ac1a773bacec3ae4874fd8ec64bc8ca862db6"));
+        	assert(hashGenesisBlock == uint256("0x00000427198483f7776f4a6b1e4afaeaa9adeed6ceaf0d137747080f6665a442"));
+        	assert(genesis.hashMerkleRoot == uint256("0x1fbd576f57ed6765eac3c705721ac1a773bacec3ae4874fd8ec64bc8ca862db6"));
 	}
 
         vSeeds.push_back(CDNSSeedData("prufus one", "139.99.105.67"));
