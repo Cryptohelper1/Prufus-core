0836704c4125f30af1cd5ec066762804d3cbb9ef
altcuim <thien.atcvn@gmail.com>
2018-06-09 23:35:50 +0700

    modified:   configure.ac 	modified:   share/qt/Info.plist 	modified:   share/qt/Info.plist.in 	modified:   share/setup.nsi.in 	modified:   src/Makefile.am 	modified:   src/Makefile.qt.include 	deleted:    src/config/prufus-config.h 	deleted:    src/config/prufus-config.h.in 	modified:   src/config/stamp-h1 	deleted:    src/prufus-cli-res.rc 	deleted:    src/prufus-cli.cpp 	deleted:    src/prufus-tx.cpp 	deleted:    src/prufusd-res.rc 	deleted:    src/prufusd.cpp 	deleted:    src/qt/prufus.cpp 	deleted:    src/qt/prufus.qrc 	deleted:    src/qt/prufus_locale.qrc 	modified:   src/qt/res/icons/bitcoin.ico 	modified:   src/qt/res/icons/bitcoin.png 	deleted:    src/qt/res/prufus-qt-res.rc 	modified:   src/util.cpp 	src/Makefile 	src/Makefile.in 	src/plexus-cli-res.rc 	src/plexus-cli.cpp 	src/plexus-tx.cpp 	src/plexusd-res.rc 	src/plexusd.cpp 	src/qt/plexus.cpp 	src/qt/plexus.qrc 	src/qt/plexus_locale.qrc 	src/qt/qrc_prufus.cpp 	src/qt/qrc_prufus_locale.cpp 	src/qt/res/images/plexus_logo_horizontal.png 	src/qt/res/plexus-qt-res.rc

--- src/util.cpp
+++ src/util.cpp
@@ -2,12 +2,12 @@
 // Copyright (c) 2009-2014 The Bitcoin developers
 // Copyright (c) 2014-2015 The Dash developers
 // Copyright (c) 2015-2017 The PIVX developers
-// Copyright (c) 2017 The Prufus developers
+// Copyright (c) 2017 The Plexus developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #if defined(HAVE_CONFIG_H)
-#include "config/prufus-config.h"
+#include "config/Plexus-config.h"
 #endif
 
 #include "util.h"
@@ -106,7 +106,7 @@ std::string to_internal(const std::string&);
 
 using namespace std;
 
-//PRUFUS only features
+//Plexus only features
 bool fMasterNode = false;
 string strMasterNodePrivKey = "";
 string strMasterNodeAddr = "";
@@ -232,8 +232,8 @@ bool LogAcceptCategory(const char* category)
             const vector<string>& categories = mapMultiArgs["-debug"];
             ptrCategory.reset(new set<string>(categories.begin(), categories.end()));
             // thread_specific_ptr automatically deletes the set when the thread ends.
-            // "prufus" is a composite category enabling all PRUFUS-related debug output
-            if (ptrCategory->count(string("prufus"))) {
+            // "Plexus" is a composite category enabling all Plexus-related debug output
+            if (ptrCategory->count(string("Plexus"))) {
                 ptrCategory->insert(string("obfuscation"));
                 ptrCategory->insert(string("swifttx"));
                 ptrCategory->insert(string("masternode"));
@@ -397,7 +397,7 @@ static std::string FormatException(std::exception* pex, const char* pszThread)
     char pszModule[MAX_PATH] = "";
     GetModuleFileNameA(NULL, pszModule, sizeof(pszModule));
 #else
-    const char* pszModule = "prufus";
+    const char* pszModule = "Plexus";
 #endif
     if (pex)
         return strprintf(
@@ -418,13 +418,13 @@ void PrintExceptionContinue(std::exception* pex, const char* pszThread)
 boost::filesystem::path GetDefaultDataDir()
 {
     namespace fs = boost::filesystem;
-// Windows < Vista: C:\Documents and Settings\Username\Application Data\PRUFUS
-// Windows >= Vista: C:\Users\Username\AppData\Roaming\PRUFUS
-// Mac: ~/Library/Application Support/PRUFUS
-// Unix: ~/.prufus
+// Windows < Vista: C:\Documents and Settings\Username\Application Data\Plexus
+// Windows >= Vista: C:\Users\Username\AppData\Roaming\Plexus
+// Mac: ~/Library/Application Support/Plexus
+// Unix: ~/.Plexus
 #ifdef WIN32
     // Windows
-    return GetSpecialFolderPath(CSIDL_APPDATA) / "PRUFUS";
+    return GetSpecialFolderPath(CSIDL_APPDATA) / "PLEXUS";
 #else
     fs::path pathRet;
     char* pszHome = getenv("HOME");
@@ -436,10 +436,10 @@ boost::filesystem::path GetDefaultDataDir()
     // Mac
     pathRet /= "Library/Application Support";
     TryCreateDirectory(pathRet);
-    return pathRet / "PRUFUS";
+    return pathRet / "PLEXUS";
 #else
     // Unix
-    return pathRet / ".prufus";
+    return pathRet / ".plexus";
 #endif
 #endif
 }
@@ -486,7 +486,7 @@ void ClearDatadirCache()
 
 boost::filesystem::path GetConfigFile()
 {
-    boost::filesystem::path pathConfigFile(GetArg("-conf", "prufus.conf"));
+    boost::filesystem::path pathConfigFile(GetArg("-conf", "Plexus.conf"));
     if (!pathConfigFile.is_complete())
         pathConfigFile = GetDataDir(false) / pathConfigFile;
 
@@ -505,7 +505,7 @@ void ReadConfigFile(map<string, string>& mapSettingsRet,
 {
     boost::filesystem::ifstream streamConfig(GetConfigFile());
     if (!streamConfig.good()) {
-        // Create empty prufus.conf if it does not exist
+        // Create empty Plexus.conf if it does not exist
         FILE* configFile = fopen(GetConfigFile().string().c_str(), "a");
         if (configFile != NULL)
             fclose(configFile);
@@ -516,7 +516,7 @@ void ReadConfigFile(map<string, string>& mapSettingsRet,
     setOptions.insert("*");
 
     for (boost::program_options::detail::config_file_iterator it(streamConfig, setOptions), end; it != end; ++it) {
-        // Don't overwrite existing settings so command line settings override prufus.conf
+        // Don't overwrite existing settings so command line settings override Plexus.conf
         string strKey = string("-") + it->string_key;
         string strValue = it->value[0];
         InterpretNegativeSetting(strKey, strValue);
@@ -531,7 +531,7 @@ void ReadConfigFile(map<string, string>& mapSettingsRet,
 #ifndef WIN32
 boost::filesystem::path GetPidFile()
 {
-    boost::filesystem::path pathPidFile(GetArg("-pid", "prufusd.pid"));
+    boost::filesystem::path pathPidFile(GetArg("-pid", "Plexusd.pid"));
     if (!pathPidFile.is_complete()) pathPidFile = GetDataDir() / pathPidFile;
     return pathPidFile;
 }
